c642dc874348735b40539e7b1481d08b092f04c4
Vitor-16 <vitor.slima1616@gmail.com>
2023-04-06 00:03:54 -0300

    alteração rotas

--- routes/routesCliente.js
+++ routes/routesCliente.js
@@ -1,7 +1,5 @@
 //IMPORTANDO PACOTE EXPRESS PARA INDEXJS
-const { response } = require('express');
 const express = require('express');
-const { model } = require('../database/database');
 
 //UTILIZANDO MODEL CLIENTE CRIADA
 const modelCliente = require('../models/modelCliente');
@@ -15,9 +13,9 @@ const router = express.Router();
 router.post("/CadastrarCliente", (req, res)=>{
     console.log(req.body);
 
-    let{nome_Cliente, email_Cliente, senha_Cliente} = req.body;
+    let{nome_Cliente} = req.body;
     modelCliente.create(
-        {nome_Cliente, email_Cliente, senha_Cliente},
+        {nome_Cliente},
         )
     .then(
         ()=>{
@@ -110,29 +108,27 @@ router.get("/ListarClienteNOME/:nome_Cliente", (req, res)=>{
 });
 
 //ROTA DE UPDATE
-router.put('/AlterarCliente/:id_Cliente', (req, res)=>{
-    const{id_Cliente, nome_Cliente, email_Cliente, senha_Cliente} = req.body;
+router.put("/AlterarCliente", (req, res)=>{
+    const {id_Cliente, nome_Cliente} = req.body;
     modelCliente.update(
-        {nome_Cliente, email_Cliente, senha_Cliente},
+        {nome_Cliente},
         {where:{id_Cliente}}
-    )
-    .then(
+    ).then(
         ()=>{
             return res.status(200).json({
                 erroStatus:false,
-                mensagemStatus:"DADOS DE CLIENTE ALTERADO COM SUCESSO."
-            });
+                mensagemStatus:"DADO DO CLIENTE ALTERADO COM SUCESSO."
+            })
         }
-    )
-    .catch(
+    ).catch(
         (error)=>{
             return res.status(400).json({
                 erroStatus:true,
-                mensagemStatus:"ERRO AO ALTERAR DADOS DE CLIENTE.",
+                mensagemStatus:"ERRO AO ALTERAR DADO DO CLIENTE.",
                 errorObject:error
             });
         }
-    )
+    );
 });
 
 //ROTA DE DELETE
